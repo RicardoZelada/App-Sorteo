535f5227eeaef5d2d9446d2ca8c4c345a0e85e64
RicardoZelada <72114080+RicardoZelada@users.noreply.github.com>
2024-04-30 12:55:41 -0400

    Creación de lógica JS y corrección de HTML
    Se realiza la función de número aleatorio y se corrigen algunas etiquetas HTML

--- Js/app.js
+++ Js/app.js
@@ -1,6 +1,4 @@
 
-    
-
 
 window.show__Errors = (error__List)=>{
     let error = document.querySelector(".div__alert");
@@ -10,38 +8,70 @@ window.show__Errors = (error__List)=>{
     error__List.forEach(element => {
         let div__toast = document.createElement("div");
         div__toast.innerText = element;
-
         toast__Container.appendChild(div__toast);
     }); 
     error.appendChild(toast__Container);
 }
 
+window.show__Winner = (result__List)=>{
+    let winner = document.querySelector(".div__result");
+    winner.classList.add("mt-2","card","bg-body","rounded");
+    let winner__Container = document.createElement("div");
+    winner__Container.classList.add("p-2");
+    result__List.forEach(item=>{
+        let div__Win = document.createElement("div");
+        div__Win.classList.add("d-flex","justify-content-center","align-items-center","fw-bold","text-danger");
+        let text = document.createElement("h6");
+        text.classList.add("ms-4","lh-1", "mb-0");
+        text.innerText = "Es el número ganador";
+        div__Win.innerText = item;
+        div__Win.appendChild(text);
+        winner__Container.appendChild(div__Win);
+    });
+    winner.appendChild(winner__Container);
+}
 
 window.register__List = []; /* Creo un array */
+window.winner__List = []; /* Creo un array */
 
+
+/* Capturo el Id del boton para realizar todo el proceso */
 const btnAux__sortear =  document.getElementById("btn__sortear");
 
 btnAux__sortear.addEventListener('click', ()=>{
    let alert_Toast = document.querySelector(".div__alert");
    alert_Toast.innerHTML = "";
-
+ 
     let data__Number = document.getElementById("floatingInput").value;
-    
+
     console.log(data__Number);
-    
+        
     error__List = []; /* Creo un array para agregar los errores */
+    result__List = []; /* Creo un array para agregar el numero ganador */
+    let number__Win = 1;
 
     if (data__Number === ""){
         error__List.push("Ingrese dato númerico");
-    }
-    if(error__List.length === 0){
-        let register__List = {} /* Creo un objeto */
-        register__List.number = data__Number;
+        }else{
+            
+            number__Win = Math.floor(Math.random()*data__Number);
+            result__List.push(number__Win);
 
-        window.register__List.push(register__List);
-    }else{
-        window.show__Errors(error__List);
-    }
+            let winner__List = {}/* Creo un objeto */
+            winner__List.win = number__Win;
+
+            window.winner__List.push(winner__List);
+            window.show__Winner(result__List);
+
+            }
+                if(error__List.length === 0){
+                    let register__List = {} /* Creo un objeto */
+                    register__List.number = data__Number;
+
+                    window.register__List.push(register__List);
+                }else{
+                    window.show__Errors(error__List);
+                }
 
     //console.log(error__List);
     //console.log(register__List);
